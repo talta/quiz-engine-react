bbe31c5b643bc1c5b0d44de2205aaf6fb5c5cd66
talta <t.taltallc@gmail.com>
2017-04-16 19:42:02 -0400

    additional tests for rendering the questionsAnswers

--- src/__test__/question-answers.test.js
+++ src/__test__/question-answers.test.js
@@ -1,3 +1,6 @@
+////tests fail to run: https://github.com/facebook/jest/issues/3114
+
+
 import React from 'react';
 import {shallow} from 'enzyme';
 
@@ -12,5 +15,17 @@ describe('QuestionAnswer', ()=>{
 		const question = 'foo';
 		const wrapper = shallow(<QuestionAnswer />);
 		expect(wrapper.contains(<h3> Question: {question}</h3>)).toEqual(true);
+	});
+	it('should render the answers', ()=>{
+		const answers = [{message: 'first answer'}] 
+		const wrapper = shallow(<QuestionAnswer />);
+		expect(wrapper.contains(<input><label>{answers.message}</label></input>))toEqual(true);
+	});
+	it('should have the number of inputs equal to the number of items within the answers array', ()=>{
+		const answers = [{message: 'first answer'}, {message: 'second answer'}];
+		const wrapper = shallow(<QuestionAnswer />);
+		const instance = wrapper.instance();
+		answers.forEach(instance.answers);
+		expect(wrapper.state('quiz').length).toEqual(answers.length);
 	});
 })
