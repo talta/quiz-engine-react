a53dbf0ef7ee6b616aaa6d0371c4413afc5f9537
talta <t.taltallc@gmail.com>
2017-04-16 22:23:56 -0400

    tests for footer

---
+++ src/__test__/footer.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+
+import Footer from '../components/footer';
+
+describe('Footer', ()=>{
+	it('should render without crashing', ()=>{
+		shallow(<Footer />);
+	});
+	// it('should contain 3 li', ()=>{
+
+	// })
+})
