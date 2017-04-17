39aa15d04cc770701970db2d8712dfda4e2187e0
talta <t.taltallc@gmail.com>
2017-04-16 20:26:13 -0400

    additional links added to the header

--- src/components/header.js
+++ src/components/header.js
@@ -9,7 +9,10 @@ export default class Header extends React.Component{
 			<header >
 				<ul className='topnav'>
 					<li> TALTA's Quiz Engine </li>
-
+					<li><a class="active" href="#home">Home</a></li>
+  					<li><a href="#news">News</a></li>
+  					<li><a href="#contact">Contact</a></li>
+  					<li class="right"><a href="#about">About</a></li>
 				</ul>
 			</header>
 		)
