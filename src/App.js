68724a2449b4bd6747c1f5edad8545a468e2288d
talta <t.taltallc@gmail.com>
2017-04-17 23:03:11 -0400

    persisting the users to the state and conditional rendering

--- src/App.js
+++ src/App.js
@@ -6,14 +6,31 @@ import Footer from './components/footer';
 import Quiz from './components/quiz';
 import User from './components/user';
 
+
  export default function App(props){
-  return (
-  	<div>
-  		<Header />
-  		<User />
-        <Footer />
-    </div>
-    )
+ // export class App extends React.Component {
+ 		///read from local storage 
+ 		const user = localStorage.getItem('User');
+ 		///if nothing returned, display user
+ 		if(typeof user !== 'undefined' && user !== null){
+ 			return  (
+  				<div>
+  					<Header />
+  					<User />
+        			<Footer />
+    			</div>
+    		)
+    	///if there is a user, get the user's score and render the quiz's
+ 		} 
+ 			////get the user's score
+ 			console.log('user found');
+	 		return(
+				<div>
+					<Header />
+					<Quiz />
+			    	<Footer />
+				</div>
+			)
 };
 
 
--- src/store.js
+++ src/store.js
@@ -3,17 +3,27 @@
 
 
 import {applyMiddleware, createStore, combineReducers} from 'redux';
-
+import throttle from 'lodash/throttle';
 import thunk from 'redux-thunk';
 
 import {quizReducer} from './reducers';
 
+import { loadState, saveState } from './helpers/localStorage';
 
 
-////if multiple reducers, use the combineReducers
-///list the additional reducers within the object
+////authentication through loadedState:
+const persistedState = loadState();
 
-export default createStore(combineReducers({quizReducer}), applyMiddleware(thunk));
+const store = createStore(
+	combineReducers({quizReducer}), 
+	applyMiddleware(thunk),
+	persistedState
+);
+
+store.subscribe(throttle(() => saveState({
+  auth: store.getState().auth
+}), 1000));
 
 
-// export default createStore(quizReducer, applyMiddleware(thunk));
+export default {store};
+
