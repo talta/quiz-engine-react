49ed902a231195be805737054c4ad9e29e8619ec
talta <t.taltallc@gmail.com>
2017-04-30 11:05:44 -0400

    API corrected

--- config.js
+++ config.js
@@ -2,4 +2,4 @@
 
 exports.APITest= (process.env.APITest || 'http://localhost:8080');
 
-exports.API=(process.env.API || 'https://ancient-bayou-82562.herokuapp.com/');
+exports.API=(process.env.API || 'https://ancient-bayou-82562.herokuapp.com');
