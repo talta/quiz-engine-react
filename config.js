5fe7ec319379c7fcd4f13e13fa620e8bc178611c
talta <t.taltallc@gmail.com>
2017-04-30 09:34:14 -0400

    API and APITest exports

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
 
 
-exports.API= (process.env.API || 'http://localhost:8080');
+exports.APITest= (process.env.APITest || 'http://localhost:8080');
+
+exports.API=(process.env.API || 'https://ancient-bayou-82562.herokuapp.com/');
