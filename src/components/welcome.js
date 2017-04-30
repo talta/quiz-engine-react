9f1a6db92ea00f306ebe0a28bf3ca8eca5fbd7d3
talta <t.taltallc@gmail.com>
2017-04-30 00:06:06 -0400

    clean up of selected quiz

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -10,7 +10,7 @@ export class Welcome extends React.Component {
     constructor(props, context){
         super(props);
         this.router=context.router;
-        // this.handleChange=this.handleChange.bind(this);
+        this.handleChange=this.handleChange.bind(this);
         this.handleSelectedQuiz=this.handleSelectedQuiz.bind(this);
     }
     
@@ -23,76 +23,25 @@ export class Welcome extends React.Component {
         else{
             this.router.history.push('/username');
         } 
-        console.log('component will be mount');
-    }
-
-    componentDidMount(){
-        let selected;
-        console.log('selected Quiz?: ', this.props.quizzes);
-        // selected = this.props.quizzes[0].name;
-        console.log('selected: ', selected);
-
-        // if(this.props.value!== undefined){
-        //     console.log('selected in else: ', this.props.value);
-            // selected = this.props.value;
-        // }else{
-        //     console.log('selected Quiz is default: ', this.props.quizzes[0].name);
-            // selected = this.props.quizzes[0].name;
-        // }
-
-
-        // if (this.props.selectedQuiz==='' || this.props.value=== undefined) {
-        //     console.log('selected Quiz is default: ', this.props.quizzes[0].name);
-        //     selected = this.props.quizzes[0].name;
-        // }else {
-        //     console.log('selected in else: ', this.props.value);
-        //     selected = this.props.value;
-        // }
-        // this.props.dispatch(selectQuiz(selected));
-        console.log('Welcome Component Selected Quiz Prop: ', this.props.selectedQuiz);
     }
 
     handleSelectedQuiz(event){
          event.preventDefault();
-        console.log('handle selected quiz', this.props.selectedQuiz);
         if(isEmpty(this.props.selectedQuiz)){
-            console.log('theresa set the default quiz here');
-            console.log('defaulted quiz: ', this.props.quizzes[0])
             this.props.dispatch(selectQuiz(this.props.quizzes[0]));
-            console.log('selected quiz after dispatch: ', this.props.selectedQuiz);
         }
-        let selectedQuiz = this.props.selectedQuiz;
-	    let foundQuizIndex = findIndex(this.props.quizzes, function(i){return i.name=== selectedQuiz})
-	    let currentQuiz = this.props.quizzes[foundQuizIndex];
-	    // this.props.dispatch(selectQuiz(currentQuiz));
         this.router.history.push({
              pathname: `/quiz`,
              state:{selectedQuiz: this.props.selectedQuiz}
          });
      }
-    
-    getSelectedFromProps(props) {
-        console.log('get selected from props called');
-        let selected;
-        console.log('Welcome Props.value', this.props);
-        if (this.props.selectedQuiz==='' || this.props.selectedQuiz=== {}) {
-            console.log('the first quiz was selected');
-            // selected = this.props.quizzes[0].name;
-            console.log('selected Quiz: ', this.props.quizzes[0]);
-            console.log('selected within if: ', selected);
-        } else {
-            selected = this.props.value;
-        }
-        console.log('selected: ', selected);
-        // this.props.dispatch(selectQuiz(selected));
-    }
 
     handleChange(e) {
-        let selected;
-        console.log('selected in handler: ', selected);
-        console.log('this in handler: ', this);
-        this.props.dispatch(selectQuiz(selected));
-        console.log('Quiz props : ', this.props)
+        let selected= e.target.value;
+        let foundQuizIndex = findIndex(this.props.quizzes, function(i){return i.name=== selected})
+        let currentQuiz = this.props.quizzes[foundQuizIndex];
+
+        this.props.dispatch(selectQuiz(currentQuiz));
     }
 
     render() {
