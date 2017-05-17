ebe3b96e9dfd248bb7f7cab62787e35ceab7e82b
talta <t.taltallc@gmail.com>
2017-05-16 23:20:30 -0400

    welcome styling

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -78,10 +78,10 @@ export class Welcome extends React.Component {
         }
 
         return (
-            <div>
+            <div className="welcome">
                 <div>
-                     <h2>Welcome {this.props.username},
-                     </h2>
+                    <h2>Welcome {this.props.username},
+                    </h2>
                 </div>
 	            <form onSubmit={this.handleSelectedQuiz} id='selectQuiz'>
 	                <label> Test your chops with one of the following quizzes: 
